1ba40e8c8e9326fc020c22eb680372df5cf3fbca
Pol Imbing <uraman2000@gmail.com>
2019-11-18 13:43:59 +0800

    summary UI TEMP

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import ApiRespository from "./Library/InventoryApiRespository";
 import { Container } from "@material-ui/core";
 import InventoryTable from "./Components/InventoryTable";
 import Login from "./Components/Login";
+import SummaryStatus from "./Components/SummaryStatus";
 
 function App() {
   const [headCell, setheadCell] = useState([]);
@@ -21,7 +22,12 @@ function App() {
     <div>
       <Container>
         {/* <InventoryTable headCell={headCell} dataCell={dataCell} /> */}
-        <Login />
+        {/* <Login /> */}
+        <SummaryStatus tableName={"check_deposite_status"} />
+        <SummaryStatus tableName={"account_status"} />
+        <SummaryStatus tableName={"client_check_status"} />
+        <SummaryStatus tableName={"reason_for_bounce_status"} />
+        <SummaryStatus tableName={"reson_for_hold_status"} />
       </Container>
     </div>
   );
---
+++ src/Components/SummaryStatus.tsx
@@ -0,0 +1,97 @@
+import React, { useEffect, useState } from "react";
+
+import SummaryApiRepository from "../Library/SummaryApiRepository";
+import {
+  Card,
+  Typography,
+  CardContent,
+  Paper,
+  TableHead,
+  TableRow,
+  TableCell,
+  Table,
+  TableBody
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  card: {
+    width: 275
+  },
+  root: {
+    width: "100%",
+    marginTop: 6,
+    overflowX: "auto",
+    marginBottom: 6
+  },
+  table: {
+    minWidth: 250
+  },
+  totalRow: {
+    "& td": {
+      fontWeight: 500
+    }
+  }
+});
+
+export default function SummaryStatus(props: ISummaryProps) {
+  let { tableName } = props;
+  const classes = useStyles();
+  const [state, setstate] = useState<ISummaryStatusState>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await setstate(await SummaryApiRepository.getSummary(tableName));
+    };
+    fetchData();
+  }, []);
+
+  if (state) {
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table} aria-label="spanning table">
+          <TableHead>
+            <TableRow>
+              <TableCell>CLIENT ACCOUNT STATUS</TableCell>
+              <TableCell>COUNT</TableCell>
+              <TableCell>%</TableCell>
+              <TableCell>AMOUNT</TableCell>
+              <TableCell>%</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {state.statusItem.map((item: any) => (
+              <TableRow>
+                <TableCell>{item.status}</TableCell>
+                <TableCell>{item.count}</TableCell>
+                <TableCell>{item.countPercentage}</TableCell>
+                <TableCell>{item.amount}</TableCell>
+                <TableCell>{item.amountPercentage}</TableCell>
+              </TableRow>
+            ))}
+            <TableRow className={classes.totalRow}>
+              <TableCell>TOTAL</TableCell>
+              <TableCell>{state.totalCount}</TableCell>
+              <TableCell>{state.totalCountPercent}</TableCell>
+              <TableCell>{state.totalAmount}</TableCell>
+              <TableCell>{state.totalAmountPercent}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
+  return <div></div>;
+}
+
+interface ISummaryStatusState {
+  totalCount: number;
+  totalAmount: number;
+  statusItem: any[];
+  totalCountPercent: string;
+  totalAmountPercent: string;
+}
+
+interface ISummaryProps {
+  tableName: string;
+}
